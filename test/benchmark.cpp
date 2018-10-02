2fd76f016350690bf0a0af8958cc63514b7fa00b
Marek Petrik <marekpetrik@gmail.com>
2018-10-02 13:49:06 -0400

    fixed benchmarks.cpp

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -21,12 +21,10 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "craam/GMDP.hpp"
+#include "craam/MDP.hpp"
 #include "craam/Samples.hpp"
 #include "craam/algorithms/nature_response.hpp"
-#include "craam/algorithms/robust_values.hpp"
-#include "craam/algorithms/values.hpp"
-#include "craam/modeltools.hpp"
+#include "craam/solvers.hpp"
 
 // library for processing command-line options
 #include "cxxopts/cxxopts.hpp"
@@ -43,233 +41,227 @@ using namespace craam;
 
 enum class Solver { VI, MPI };
 
-void solve_mdp(const cxxopts::Options &options, Solver solver) {
-  cout << "Loading ... " << endl;
+void solve_mdp(const cxxopts::Options& options, Solver solver) {
+    cout << "Loading ... " << endl;
 
-  // open file
-  ifstream ifs;
-  ifs.open(options["input"].as<string>());
-  if (!ifs.is_open()) {
-    cout << "Failed to open the input file." << endl;
-    terminate();
-  }
-
-  // solve the MDP
-  MDP mdp = mdp_from_csv(ifs);
-  ifs.close();
-
-  // parse computation-related options
-  auto iterations = options["iterations"].as<unsigned long>();
-  prec_t discount = options["discount"].as<prec_t>();
-  prec_t precision = options["precision"].as<prec_t>();
-
-  const auto ambiguity = options["ambiguity"].as<std::string>();
-
-  cout << "Computing solution ... " << endl;
-  auto start = std::chrono::high_resolution_clock::now();
-
-  // output values
-  int iters;
-  prec_t residual;
-  indvec policy;
-  numvec valuefunction;
-
-  if (ambiguity.empty()) {
-    algorithms::DetermSolution sol;
-    if (solver == Solver::MPI) {
-      sol = algorithms::solve_mpi(mdp, discount, numvec(0), indvec(0),
-                                  iterations, precision, iterations, 0.5);
-    } else if (solver == Solver::VI) {
-      sol = algorithms::solve_vi(mdp, discount, numvec(0), indvec(0),
-                                 iterations, precision);
-    } else {
-      throw invalid_argument("Unknown solver type.");
+    // open file
+    ifstream ifs;
+    ifs.open(options["input"].as<string>());
+    if (!ifs.is_open()) {
+        cout << "Failed to open the input file." << endl;
+        terminate();
     }
-    iters = sol.iterations;
-    residual = sol.residual;
-    policy = move(sol.policy);
-    valuefunction = move(sol.valuefunction);
 
-  } else if (ambiguity == "L1") {
-    algorithms::SARobustSolution sol;
+    // solve the MDP
+    MDP mdp = mdp_from_csv(ifs);
+    ifs.close();
 
-    prec_t budget = options["budget"].as<prec_t>();
-    numvecvec budgets = map_sa<prec_t>(
-        mdp, [budget](const State &, const Action &) { return budget; });
+    // parse computation-related options
+    auto iterations = options["iterations"].as<unsigned long>();
+    prec_t discount = options["discount"].as<prec_t>();
+    prec_t precision = options["precision"].as<prec_t>();
 
-    if (solver == Solver::MPI) {
-      sol = algorithms::rsolve_mpi(
-          mdp, discount, algorithms::nats::robust_l1(budgets), numvec(0),
-          indvec(0), iterations, precision, MAXITER, precision);
-    } else if (solver == Solver::VI) {
-      sol = algorithms::rsolve_vi(mdp, discount,
-                                  algorithms::nats::robust_l1(budgets),
-                                  numvec(0), indvec(0), iterations, precision);
-    } else {
-      throw invalid_argument("Unknown solver type.");
+    const auto ambiguity = options["ambiguity"].as<std::string>();
+
+    cout << "Computing solution ... " << endl;
+    auto start = std::chrono::high_resolution_clock::now();
+
+    // output values
+    int iters;
+    prec_t residual;
+    indvec policy;
+    numvec valuefunction;
+
+    if (ambiguity.empty()) {
+        algorithms::DetermSolution sol;
+        if (solver == Solver::MPI) {
+            sol = algorithms::solve_mpi(mdp, discount, numvec(0), indvec(0), iterations,
+                                        precision, iterations, 0.5);
+        } else if (solver == Solver::VI) {
+            sol = algorithms::solve_vi(mdp, discount, numvec(0), indvec(0), iterations,
+                                       precision);
+        } else {
+            throw invalid_argument("Unknown solver type.");
+        }
+        iters = sol.iterations;
+        residual = sol.residual;
+        policy = move(sol.policy);
+        valuefunction = move(sol.valuefunction);
+
+    } else if (ambiguity == "L1") {
+        algorithms::SARobustSolution sol;
+
+        prec_t budget = options["budget"].as<prec_t>();
+        numvecvec budgets =
+            map_sa<prec_t>(mdp, [budget](const State&, const Action&) { return budget; });
+
+        if (solver == Solver::MPI) {
+            sol = algorithms::rsolve_mpi(
+                mdp, discount, algorithms::nats::robust_l1(budgets), numvec(0), indvec(0),
+                iterations, precision, MAXITER, precision);
+        } else if (solver == Solver::VI) {
+            sol =
+                algorithms::rsolve_vi(mdp, discount, algorithms::nats::robust_l1(budgets),
+                                      numvec(0), indvec(0), iterations, precision);
+        } else {
+            throw invalid_argument("Unknown solver type.");
+        }
+        iters = sol.iterations;
+        residual = sol.residual;
+        policy = unzip(sol.policy).first;
+        valuefunction = move(sol.valuefunction);
     }
-    iters = sol.iterations;
-    residual = sol.residual;
-    policy = unzip(sol.policy).first;
-    valuefunction = move(sol.valuefunction);
-  }
 #ifdef GUROBI_USE
-  else if (ambiguity == "L1g") {
-    algorithms::SARobustSolution sol;
+    else if (ambiguity == "L1g") {
+        algorithms::SARobustSolution sol;
 
-    prec_t budget = options["budget"].as<prec_t>();
-    numvecvec budgets = map_sa<prec_t>(
-        mdp, [budget](const State &, const Action &) { return budget; });
+        prec_t budget = options["budget"].as<prec_t>();
+        numvecvec budgets =
+            map_sa<prec_t>(mdp, [budget](const State&, const Action&) { return budget; });
 
-    if (solver == Solver::MPI) {
-      sol = algorithms::rsolve_mpi(
-          mdp, discount, algorithms::nats::robust_l1w_gurobi(budgets),
-          numvec(0), indvec(0), iterations, precision, MAXITER, precision);
-    } else if (solver == Solver::VI) {
-      sol = algorithms::rsolve_vi(mdp, discount,
-                                  algorithms::nats::robust_l1w_gurobi(budgets),
-                                  numvec(0), indvec(0), iterations, precision);
-    } else {
-      throw invalid_argument("Unknown solver type.");
+        if (solver == Solver::MPI) {
+            sol = algorithms::rsolve_mpi(
+                mdp, discount, algorithms::nats::robust_l1w_gurobi(budgets), numvec(0),
+                indvec(0), iterations, precision, MAXITER, precision);
+        } else if (solver == Solver::VI) {
+            sol = algorithms::rsolve_vi(mdp, discount,
+                                        algorithms::nats::robust_l1w_gurobi(budgets),
+                                        numvec(0), indvec(0), iterations, precision);
+        } else {
+            throw invalid_argument("Unknown solver type.");
+        }
+        iters = sol.iterations;
+        residual = sol.residual;
+        policy = unzip(sol.policy).first;
+        valuefunction = move(sol.valuefunction);
     }
-    iters = sol.iterations;
-    residual = sol.residual;
-    policy = unzip(sol.policy).first;
-    valuefunction = move(sol.valuefunction);
-  }
 #endif
-  else {
-    throw invalid_argument("Unknown uncertainty set type.");
-  }
-
-  auto finish = chrono::high_resolution_clock::now();
-  cout << "Done computing." << endl;
-  cout << "Iterations: " << iters << ", Residual: " << residual << endl;
-  std::cout
-      << "Duration:  *** "
-      << chrono::duration_cast<chrono::milliseconds>(finish - start).count()
-      << "ms *** \n";
-
-  // only do this when an output file has been provided
-  if (options.count("output") > 0) {
-    cout << "Writing output ... " << endl;
-    ofstream ofs;
-    ofs.open(options["output"].as<string>());
-    if (!ofs.is_open()) {
-      cout << "Could not open the output file for writing" << endl;
-      terminate();
+    else {
+        throw invalid_argument("Unknown uncertainty set type.");
     }
-    ofs << "idstate,idaction,value" << endl;
-    for (size_t i = 0; i < mdp.state_count(); i++) {
-      ofs << i << "," << policy[i] << "," << valuefunction[i] << endl;
+
+    auto finish = chrono::high_resolution_clock::now();
+    cout << "Done computing." << endl;
+    cout << "Iterations: " << iters << ", Residual: " << residual << endl;
+    std::cout << "Duration:  *** "
+              << chrono::duration_cast<chrono::milliseconds>(finish - start).count()
+              << "ms *** \n";
+
+    // only do this when an output file has been provided
+    if (options.count("output") > 0) {
+        cout << "Writing output ... " << endl;
+        ofstream ofs;
+        ofs.open(options["output"].as<string>());
+        if (!ofs.is_open()) {
+            cout << "Could not open the output file for writing" << endl;
+            terminate();
+        }
+        ofs << "idstate,idaction,value" << endl;
+        for (size_t i = 0; i < mdp.state_count(); i++) {
+            ofs << i << "," << policy[i] << "," << valuefunction[i] << endl;
+        }
+        ofs.close();
     }
-    ofs.close();
-  }
-  cout << "Done." << endl;
+    cout << "Done." << endl;
 }
 
-void build_mdp(const cxxopts::Options &options) {
+void build_mdp(const cxxopts::Options& options) {
 
-  // load samples (reward is received after state and action in the same line)
-  io::CSVReader<4> in(options["input"].as<string>());
-  in.read_header(io::ignore_extra_column, "step", "idstate", "idaction",
-                 "reward");
+    // load samples (reward is received after state and action in the same line)
+    io::CSVReader<4> in(options["input"].as<string>());
+    in.read_header(io::ignore_extra_column, "step", "idstate", "idaction", "reward");
 
-  cout << "Loading samples ... " << endl;
-  craam::msen::DiscreteSamples samples;
+    cout << "Loading samples ... " << endl;
+    craam::msen::DiscreteSamples samples;
 
-  // values from the last sample
-  int last_step, last_state, last_action;
-  double last_reward;
+    // values from the last sample
+    int last_step, last_state, last_action;
+    double last_reward;
 
-  if (!in.read_row(last_step, last_state, last_action, last_reward)) {
-    cout << "Only one sample ... cannot do anything with that. " << endl;
-    terminate();
-  }
-
-  int run = 0;
-  // values from the current sample
-  int step, state, action;
-  double reward;
-
-  while (in.read_row(step, state, action, reward)) {
-    if (step == last_step + 1) {
-      // this means we are not at the boundary between episodes, we can add the
-      // sample
-      samples.add_sample(last_state, last_action, state, last_reward, 1.0, step,
-                         run);
-    } else { // else we just skip the sample
-      run++;
+    if (!in.read_row(last_step, last_state, last_action, last_reward)) {
+        cout << "Only one sample ... cannot do anything with that. " << endl;
+        terminate();
     }
-    last_step = step;
-    last_state = state;
-    last_action = action;
-    last_reward = reward;
-  }
 
-  cout << "Loaded " << samples.size() << " samples " << endl;
+    int run = 0;
+    // values from the current sample
+    int step, state, action;
+    double reward;
 
-  cout << "Building MDP ... " << endl;
-  craam::msen::SampledMDP smdp;
-  smdp.add_samples(samples);
+    while (in.read_row(step, state, action, reward)) {
+        if (step == last_step + 1) {
+            // this means we are not at the boundary between episodes, we can add the
+            // sample
+            samples.add_sample(last_state, last_action, state, last_reward, 1.0, step,
+                               run);
+        } else { // else we just skip the sample
+            run++;
+        }
+        last_step = step;
+        last_state = state;
+        last_action = action;
+        last_reward = reward;
+    }
 
-  if (options.count("output") > 0) {
-    cout << "Writing output ... " << endl;
-    to_csv_file(*smdp.get_mdp(), options["output"].as<string>());
-  }
+    cout << "Loaded " << samples.size() << " samples " << endl;
+
+    cout << "Building MDP ... " << endl;
+    craam::msen::SampledMDP smdp;
+    smdp.add_samples(samples);
+
+    if (options.count("output") > 0) {
+        cout << "Writing output ... " << endl;
+        to_csv_file(*smdp.get_mdp(), options["output"].as<string>());
+    }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 
-  cxxopts::Options options("craam",
-                           "Fast command-line solver for (robust) MDPs");
+    cxxopts::Options options("craam", "Fast command-line solver for (robust) MDPs");
 
-  options.add_options()("h,help", "Display help message.")(
-      "i,input", "Input file, MDP description or samples (csv file)",
-      cxxopts::value<string>())("o,output",
-                                "Policy and value output (csv file)",
-                                cxxopts::value<string>())(
-      "m,method",
-      "Solution method: MPI - Modified Policy Iteration, VI - Value Iteration, "
-      "MDP - Construct from samples",
-      cxxopts::value<string>()->default_value("MPI"))(
-      "d,discount", "Discount factor",
-      cxxopts::value<double>()->default_value("0.9"))(
-      "e,precision", "Maximum residual",
-      cxxopts::value<double>()->default_value("0.0001"))(
-      "l,iterations", "Maximum number of iterations",
-      cxxopts::value<unsigned long>()->default_value("2000"))(
-      "b,budget", "Robustness budget",
-      cxxopts::value<double>()->default_value("0.0"))(
-      "u,ambiguity", "Type of ambiguity", cxxopts::value<string>());
+    options.add_options()("h,help", "Display help message.")(
+        "i,input", "Input file, MDP description or samples (csv file)",
+        cxxopts::value<string>())("o,output", "Policy and value output (csv file)",
+                                  cxxopts::value<string>())(
+        "m,method",
+        "Solution method: MPI - Modified Policy Iteration, VI - Value Iteration, "
+        "MDP - Construct from samples",
+        cxxopts::value<string>()->default_value("MPI"))(
+        "d,discount", "Discount factor", cxxopts::value<double>()->default_value("0.9"))(
+        "e,precision", "Maximum residual",
+        cxxopts::value<double>()->default_value("0.0001"))(
+        "l,iterations", "Maximum number of iterations",
+        cxxopts::value<unsigned long>()->default_value("2000"))(
+        "b,budget", "Robustness budget", cxxopts::value<double>()->default_value("0.0"))(
+        "u,ambiguity", "Type of ambiguity", cxxopts::value<string>());
 
-  try {
-    options.parse(argc, argv);
-  } catch (const cxxopts::OptionException &oe) {
-    cout << oe.what() << endl << endl << " *** usage *** " << endl;
-    cout << options.help() << endl;
-    terminate();
-  }
+    try {
+        options.parse(argc, argv);
+    } catch (const cxxopts::OptionException& oe) {
+        cout << oe.what() << endl << endl << " *** usage *** " << endl;
+        cout << options.help() << endl;
+        terminate();
+    }
 
-  if (options["h"].as<bool>()) {
-    cout << options.help() << endl;
-    terminate();
-  }
+    if (options["h"].as<bool>()) {
+        cout << options.help() << endl;
+        terminate();
+    }
 
-  if (options.count("input") == 0) {
-    cout << "No input file provided. See usage (-h)." << endl;
-    terminate();
-  }
+    if (options.count("input") == 0) {
+        cout << "No input file provided. See usage (-h)." << endl;
+        terminate();
+    }
 
-  const auto method = options["method"].as<std::string>();
-  if (method == "MPI") {
-    solve_mdp(options, Solver::MPI);
-  } else if (method == "VI") {
-    solve_mdp(options, Solver::VI);
-  } else if (method == "MDP") {
-    build_mdp(options);
-  } else {
-    cout << "Unknown method type: " << method << "." << endl;
-    terminate();
-  }
+    const auto method = options["method"].as<std::string>();
+    if (method == "MPI") {
+        solve_mdp(options, Solver::MPI);
+    } else if (method == "VI") {
+        solve_mdp(options, Solver::VI);
+    } else if (method == "MDP") {
+        build_mdp(options);
+    } else {
+        cout << "Unknown method type: " << method << "." << endl;
+        terminate();
+    }
 }
